3124118dcd79302fa9ed0bbc237e00e532466965
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-11-28 17:42:21 +0700

    feat: create fabrics section

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-import './style.css'
+import "./style.css";
+
+document.querySelectorAll<HTMLDivElement>(".fabrics__card").forEach((element) => {
+  const img = element.dataset.img;
+  element.style.backgroundImage = `
+  linear-gradient(
+    180deg,
+    rgba(0,0,0,0) 0%,
+    rgba(0,0,0,0.5) 80%
+  ),
+  url(${img})
+  `;
+});
