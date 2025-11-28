0f49f32b369762a7f4a53c55f2d449f75978234f
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-11-28 19:06:58 +0700

    Updates

---
+++ assets/index-0K3JhfBv.js
@@ -0,0 +1,8 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function c(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=c(e);fetch(e.href,r)}})();document.querySelectorAll(".fabrics__card").forEach(o=>{const t=o.dataset.img;o.style.backgroundImage=`
+  linear-gradient(
+    180deg,
+    rgba(0,0,0,0) 0%,
+    rgba(0,0,0,0.5) 80%
+  ),
+  url(${t})
+  `});
